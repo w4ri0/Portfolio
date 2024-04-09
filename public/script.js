c933556acc7418efc04f8c31626d9fa8a96ea86b
Gabriel DAGON <148529201+w4ri0@users.noreply.github.com>
2024-04-09 13:40:36 +0000

    modified:   public/script.js 	modified:   public/style.css

--- public/script.js
+++ public/script.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', function() {
-        var glassButton = document.getElementById('glass-button');
-        if (window.scrollY > 0) {
-            glassButton.style.display = 'block';
-        } else {
-            glassButton.style.display = 'none';
-        }
-      })});
+const showMenu = document.querySelector('.show-menu');
+const menuBlock = document.querySelector('.menu-block');
+
+// Check if the elements exist
+if (showMenu && menuBlock) {
+    // Add a click event listener to show/hide the menu block
+    showMenu.addEventListener('click', function() {
+        // Toggle the visibility of the menu block
+        menuBlock.style.display = menuBlock.style.display === 'none' ? 'inline-block' : 'none';
+        menuBlock.style.opacity = menuBlock.style.display === 'none' ? '0' : '1';
+        menuBlock.style.visibility = menuBlock.style.display === 'none' ? 'hidden' : 'visible';
+        menuBlock.style.transition = 'transform 2.5s ease-in-out'; // Add transition property
+        menuBlock.style.transform = menuBlock.style.display === 'none' ? 'translateX(-100%)' : 'translateX(0)'; // Add transform property
+    });
+}
+// Add a page loading animation for 3 seconds
+window.addEventListener('load', function() {
+    const loadingAnimation = document.querySelector('.loading-animation');
+    if (loadingAnimation) {
+        setTimeout(function() {
+            loadingAnimation.style.display = 'none';
+        }, 3000);
+    }
+});
