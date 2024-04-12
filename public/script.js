de5442f5d017d149627bc62b14127bb5a6eadb60
Gabriel DAGON <148529201+w4ri0@users.noreply.github.com>
2024-04-12 10:36:26 +0000

    modified:   public/index.html 	deleted:    public/loading.png 	modified:   public/script.js 	modified:   public/style.css

--- public/script.js
+++ public/script.js
@@ -1,1 +1,28 @@
-const navUL=document.querySelector(".navUL"),showMenu=document.querySelector(".show-menu");showMenu.addEventListener("click",()=>{navUL.classList.contains("fadeout")?(navUL.classList.remove("fadeout"),navUL.classList.add("fadein"),navUL.style.display="inline-flex",console.log("fade")):navUL.classList.contains("fadein")&&(navUL.classList.remove("fadein"),navUL.classList.add("fadeout"),setTimeout(()=>{navUL.style.display="none"},400),console.log("clicked"))}),window.onload=function(){setTimeout(()=>{const loadingScreen=document.getElementById("loadingScreen");setTimeout(()=>{loadingScreen.classList.add("zoomOut")},400)},400),setTimeout(()=>{const allText=document.querySelectorAll("p","h1","h2","h3","h4","h5","h6","a","span","li","button");allText.forEach(text=>{text.classList.add("fadein")})},750),setTimeout(()=>{const loadingScreen=document.getElementById("loadingScreen");loadingScreen.style.display="none"},1100)};
+const navUL = document.querySelector(".navUL"),
+  showMenu = document.querySelector(".show-menu");
+showMenu.addEventListener("click", () => {
+  navUL.classList.contains("fadeout")
+    ? (navUL.classList.remove("fadeout"),
+      navUL.classList.add("fadein"),
+      (navUL.style.display = ""),
+      console.log("fade"))
+    : navUL.classList.contains("fadein") &&
+      (navUL.classList.remove("fadein"),
+      navUL.classList.add("fadeout"),
+      setTimeout(() => {
+        navUL.style.display = "none";
+      }, 400),
+      console.log("clicked"));
+}),
+  (window.onload = function () {
+    setTimeout(() => {
+      const loadingScreen = document.getElementById("loadingScreen");
+      setTimeout(() => {
+        loadingScreen.classList.add("zoomOut");
+      }, 400);
+    }, 400),
+      setTimeout(() => {
+        const loadingScreen = document.getElementById("loadingScreen");
+        loadingScreen.style.display = "none";
+      }, 1100);
+  });
