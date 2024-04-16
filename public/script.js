6b1c1d32c0c7f9b85235dc01a99fff5cbc4ecb29
Gabriel DAGON <148529201+w4ri0@users.noreply.github.com>
2024-04-16 09:01:10 +0000

    Refactor fadein animation duration and loading screen functionality in script.js

--- public/script.js
+++ public/script.js
@@ -1,40 +1,38 @@
 const navUL = document.querySelector(".navUL");
 const showMenu = document.querySelector(".show-menu");
-const closeButton = document.createElement(
-  "  button role='button' aria-label='button'"
-);
+const closeButton = document.createElement("button");
 
 window.onload = function () {
   setTimeout(() => {
     const loadingScreen = document.getElementById("loadingScreen");
     setTimeout(() => {
-      loadingScreen.classList.add(".zoomOut");
+      loadingScreen.classList.add("zoomOut");
     }, 400);
-  }, 400);
-
-  setTimeout(() => {
-    const allText = document.querySelectorAll([
-      "p",
-      "h1",
-      "h2",
-      "h3",
-      "h4",
-      "h5",
-      "h6",
-      "a",
-      "span",
-      "li",
-      "button",
-    ]);
-    allText.forEach((text) => {
-      text.classList.add("fadeIn");
-    });
-  }, 450);
+  }, 400),
+    setTimeout(() => {
+      const allText = document.querySelectorAll(
+        "p",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "a",
+        "span",
+        "li",
+        "button"
+      );
+      allText.forEach((text) => {
+        text.classList.add("fadein");
+      });
+    }, 750),
+    setTimeout(() => {
+      const loadingScreen = document.getElementById("loadingScreen");
+      loadingScreen.style.display = "none";
+    }, 1100);
 };
-setTimeout(() => {
-  const loadingScreen = document.getElementById("loadingScreen");
-  loadingScreen.style.display = "none";
-}, 1100);
+
 showMenu.addEventListener("click", () => {
   showMenu.style.display = "none";
   navUL.style.display = "block";
@@ -51,11 +49,9 @@ showMenu.addEventListener("click", () => {
   closeButton.style.position = "absolute";
   closeButton.style.top = "10px";
   closeButton.style.right = "10px";
+});
+closeButton.addEventListener("click", () => {
+  navUL.style.display = "none";
+  showMenu.style.display = "block";
   navUL.appendChild(closeButton);
-
-  closeButton.addEventListener("click", () => {
-    navUL.style.display = "none";
-    showMenu.style.display = "block";
-    navUL.appendChild(closeButton);
-  });
 });
