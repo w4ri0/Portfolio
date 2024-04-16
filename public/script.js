ae17c76201d415f7ee9f8c6f430c01c2967e65b0
Gabriel DAGON <148529201+w4ri0@users.noreply.github.com>
2024-04-16 09:10:42 +0000

    Refactor menu button functionality and styling in script.js and style.css

--- public/script.js
+++ public/script.js
@@ -35,7 +35,6 @@ window.onload = function () {
 
 showMenu.addEventListener("click", () => {
   navUL.appendChild(closeButton);
-  navUL.classList.remove("navUL");
   closeButton.classList.remove("close-button");
   showMenu.classList.add("show-menu-hidden");
   navUL.classList.add("navUL-modified");
@@ -46,7 +45,6 @@ showMenu.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   if (showMenu.classList.contains("show-menu-hidden")) {
     navUL.removeChild(closeButton);
-    navUL.classList.add("navUL");
     closeButton.classList.add("close-button");
     showMenu.classList.remove("show-menu-hidden");
     navUL.classList.remove("navUL-modified");
