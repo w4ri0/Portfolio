143ae809baa97b44772ad453f9e3671d5164187c
Gabriel DAGON <148529201+w4ri0@users.noreply.github.com>
2024-04-16 09:04:15 +0000

    Refactor menu button functionality and styling in script.js and style.css

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const navUL = document.querySelector(".navUL");
 const showMenu = document.querySelector(".show-menu");
-const closeButton = document.createElement("button");
+closeButton = document.createElement("button");
 
 window.onload = function () {
   setTimeout(() => {
@@ -34,24 +34,16 @@ window.onload = function () {
 };
 
 showMenu.addEventListener("click", () => {
-  showMenu.style.display = "none";
-  navUL.style.display = "block";
-  navUL.style.margin = "0 auto";
-  navUL.style.background = "white";
-  navUL.style.padding = "36%";
-  navUL.style.paddingBottom = "80%";
-  navUL.style.margin = "0";
-  navUL.style.width = "100%";
-  navUL.style.boxSizing = "border-box";
-
+  showMenu.classList.add("show-menu-hidden");
+  navUL.classList.add("navUL-modified");
+  closeButton.classList.add("close-button-modified");
   closeButton.innerText = "+";
   closeButton.classList.add("close");
-  closeButton.style.position = "absolute";
-  closeButton.style.top = "10px";
-  closeButton.style.right = "10px";
 });
 closeButton.addEventListener("click", () => {
-  navUL.style.display = "none";
-  showMenu.style.display = "block";
-  navUL.appendChild(closeButton);
+  showMenu.classList.remove("show-menu-hidden");
+  navUL.classList.remove("navUL-modified");
+  closeButton.classList.remove("close-button-modified");
+  closeButton.innerText = "";
+  closeButton.classList.remove("close");
 });
