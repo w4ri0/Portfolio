a299d15933a5809cbad26395069dbd6059235c5e
Gabriel DAGON <148529201+w4ri0@users.noreply.github.com>
2024-04-17 14:27:33 +0000

    modified:   public/js/index.js

--- public/js/index.js
+++ public/js/index.js
@@ -21,66 +21,36 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
 
-
 const username = prompt("Please Tell Us Your Name");
 
-
 function sendMessage(e) {
-
   e.preventDefault();
-
-
-  // get values to be submitted
-
   const timestamp = Date.now();
-
   const messageInput = document.getElementById("message-input");
-
   const message = messageInput.value;
-
-
-  // clear the input box
-
   messageInput.value = "";
-
-
-  //auto scroll to bottom
-
   document
-
     .getElementById("messages")
-
     .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-
-
-  // create db collection and send in the data
-
   db.ref("messages/" + timestamp).set({
-
     username,
-
     message,
-
   });
-
 }
-
 
 const fetchChat = db.ref("messages/");
 
-
 fetchChat.on("child_added", function (snapshot) {
-
   const messages = snapshot.val();
-
-  const message = `<li class=${
-
-    username === messages.username ? "sent" : "receive"
-
-  }><span>${messages.username}: </span>${messages.message}</li>`;
-
-  // append the message on the page
-
+  const message = `<li class=${username === messages.username ? "sent" : "receive"}><span>${messages.username}: </span>${messages.message}</li>`;
   document.getElementById("messages").innerHTML += message;
+});
 
+// Retrieve and display existing messages
+fetchChat.once("value", function (snapshot) {
+  snapshot.forEach(function (childSnapshot) {
+    const messages = childSnapshot.val();
+    const message = `<li class=${username === messages.username ? "sent" : "receive"}><span>${messages.username}: </span>${messages.message}</li>`;
+    document.getElementById("messages").innerHTML += message;
+  });
 });
