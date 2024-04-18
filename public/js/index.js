a09ed9812172c43c6be2f776bdb709914d55efdd
Gabriel DAGON <148529201+w4ri0@users.noreply.github.com>
2024-04-18 09:21:57 +0000

    modified:   public/devlopemmentWeb.html 	modified:   public/js/index.js

--- public/js/index.js
+++ public/js/index.js
@@ -20,39 +20,3 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
-const username = prompt("Please Tell Us Your Name");
-var msgTxt=document.getElementById("msgTxt");
-var sender;
-if(sessionStorage.getItem("username")){
-  sender=sessionStorage.getItem("username");}
-  else{
-    sender=prompt("Please Tell Us Your Name");
-    sessionStorage.setItem("username",username);
-  }
-
-module.sendMsg = function sendMsg() {
-  var msg=msgTxt.value;
-  var timestamp=new Date().getTime();
-  set(ref(db,"messages/"+timestamp),{
-    msg:msg,
-    username:username
-  });
-  }
-  onChildAdded(ref(db,"messages"), (data)=>{
-    if(data.val().sender == sender){
-        messages.innerHTML += "<div style=justify-content:end class=outer id="+data.key+"><div id=inner class=me>you : "+data.val().msg+"<button id=dltMsg onclick=module.dltMsg("+data.key+")>DELETE</button></div></div>";
-    } else {
-        messages.innerHTML += "<div class=outer id="+data.key+"><div id=inner class=notMe>"+data.val().sender+" : "+data.val().msg+"</div></div>";
-    }
-})
-
-// TO DELETE MSG
-module.dltMsg = function dltMsg(key){
-    remove(ref(db,"messages/"+key));
-}
-
-// WHEN MSG IS DELETED
-onChildRemoved(ref(db,"messages"),(data)=>{
-    var msgBox = document.getElementById(data.key);
-    messages.removeChild(msgBox);
-})
